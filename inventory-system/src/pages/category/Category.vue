18cd154034974fb54e4c49c060aaea9b07bf0d9d
Jigs <jigsterjava23@yahoo.com>
2018-06-19 09:26:56 +0800

    edit and delete functions for category

--- inventory-system/src/pages/category/Category.vue
+++ inventory-system/src/pages/category/Category.vue
@@ -1,44 +1,54 @@
 <template>
-  <categoryTable :table-headers="categoryHeaders"
-                :table-data="categories" />
+  <categoryTable
+  :table-headers = "categoryHeaders"
+  :table-data = "categories"
+  :on-edit = "editCategory" 
+  :on-delete = "deleteCategory"
+                
+                />
 </template>
 
 <script>
+import CategoryTable from "../../components/BaseTable.vue";
 
-  import CategoryTable from '../../components/BaseTable.vue'
-
-
-
-  export default {
-    name: "Category",
-    components: {
-      categoryTable: CategoryTable
+export default {
+  name: "Category",
+  components: {
+    categoryTable: CategoryTable
+  },
+  data() {
+    return {
+      categoryHeaders: ["Name"]
+    };
+  },
+  computed: {
+    categories() {
+      return this.$store.state.category.categories.map(o => {
+        return {
+          id: o.id,
+          data: [o.name]
+        };
+      });
+    }
+  },
+  created() {
+    this.axios.get("api/category/list").then(response => {
+      console.log(response.data);
+      this.$store.dispatch("setCategory", response.data);
+    });
+  },
+  methods: {
+    editCategory(categoryId) {
+      console.log("edit");
+      console.log(categoryId);
     },
-    data() {
-          return {
-           categoryHeaders: ['Name' ]
-          }
-
-    },
-    computed:{
-      categories(){
-          return this.$store.state.category.categories.map(o =>{
-            return {
-              id: o.id,
-              data:[o.name]
-            }
-          })
-      }
-    },
-    created(){
-      this.axios.get('api/category/list').then(response=>{
-        console.log(response.data)
-        this.$store.dispatch('setCategory', response.data)
-      })
+    deleteCategory(categoryId) {
+      console.log("delete");
+      console.log(categoryId);
     }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
