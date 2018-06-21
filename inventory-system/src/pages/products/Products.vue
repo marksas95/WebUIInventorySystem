ec9d314992c9dae8887c7fc13993b63313c77480
marksas <marksas_christian@yahoo.com>
2018-06-21 14:46:23 +0800

    Arrange the product saylo ang mga axios call sa store

--- inventory-system/src/pages/products/Products.vue
+++ inventory-system/src/pages/products/Products.vue
@@ -2,17 +2,17 @@
   <div>
     <appHeader></appHeader>
     <productTable v-show="!individualDetails"
-        :table-headers="tableHeaders"
-        :table-data="products"
-        :on-edit="editProduct"
-        :on-delete="deleteProduct"
-        :on-details="getProduct"
-        :on-click="onClick"/>
+                  :table-headers="tableHeaders"
+                  :table-data="products"
+                  :on-edit="editProduct"
+                  :on-delete="deleteProduct"
+                  :on-details="getProduct"
+                  :on-click="onClick"/>
 
     <product-details v-show="individualDetails"
-        :details-title="detailsTitle"
-        :objectDetails="productDetails"
-        :on-click="onClick"/>
+                     :details-title="detailsTitle"
+                     :objectDetails="productDetails"
+                     :on-click="onClick"/>
   </div>
 </template>
 
@@ -21,87 +21,84 @@
   import ProductTable from '../../components/BaseTable.vue'
   import ProductDetails from '../../components/BaseDetails'
   import {mapActions} from 'vuex'
+  import {mapGetters} from 'vuex'
+
   export default {
     data() {
       return {
-        ...mapActions[
-            'initProducts'
-          ],
         tableHeaders: ['Category', 'Item Code', 'Description', 'Unit of Measurement', 'Serial Number', 'Status'],
-        individualDetails:false,
-        productIdToPass:0,
-        productDetails:{}
+        individualDetails: false,
+        productIdToPass: 0,
+        productDetails: {}
       }
     },
     computed: {
+      ...mapGetters([
+        'GET_PRODUCT'
+      ]),
       activeProducts() {
         return this.$store.getters.getActiveProducts;
       },
       products() {
         return this.$store.state.product.products.map(p => {
-            return {
-              id:p.id,
-              data:[
-                p.category == null ? '':p.category.name,
-                p.itemCode,
-                p.description,
-                p.unitOfMeasurement,
-                p.serialNumber,
-                p.active== true? 'Active':'Not Active']
-            }
+          return {
+            id: p.id,
+            data: [
+              p.category == null ? '' : p.category.name,
+              p.itemCode,
+              p.description,
+              p.unitOfMeasurement,
+              p.serialNumber,
+              p.active == true ? 'Active' : 'Not Active']
+          }
         });
       },
-      detailsTitle(){
+      detailsTitle() {
         return 'Product Details'
       }
 
     },
-    created: function () {
-      //change to map actions
-
-      if (this.$store.state.product.products.length === 0) {
-        console.log('create')
-        this.$store.dispatch('initProducts')
-      }else {
-        console.log('wala nag create')
-      }
-
-
-    },
     methods: {
-      onClick(){
+      ...mapActions([
+        'INIT_PRODUCTS',
+        'DELETE_PRODUCT'
+      ]),
+      onClick() {
         this.individualDetails = !this.individualDetails;
         console.log(this.individualDetails)
       },
       editProduct(productID) {
         console.log('edit')
         console.log(productID)
-        this.$router.push({path:`/products/${productID}`})
+        this.$router.push({path: `/products/${productID}`})
       },
       getProduct(productId) {
-        this.axios.get('/api/product/findById?id=' + productId).then((response) => {
-          let p = response.data
-          this.productDetails = {
-            'Category': p.category == null ? '':p.category.name,
-            'Item Code': p.itemCode,
-            'Description' : p.description,
-            'Unit Of Measurement' : p.unitOfMeasurement,
-            'Serial Number' : p.serialNumber,
-            'Status' : p.active == true ? 'Active':'Not Active'
-          }
-          console.log(this.productDetails)
-        })
+        let p = this.GET_PRODUCT(productId)
+        this.productDetails = {
+          'Category': p.category == null ? '' : p.category.name,
+          'Item Code': p.itemCode,
+          'Description': p.description,
+          'Unit Of Measurement': p.unitOfMeasurement,
+          'Serial Number': p.serialNumber,
+          'MinimumStocks': p.minimumStocks,
+          'Vatable': p.vatable === true ? 'Vatable' : 'Not Vatable',
+          'Remarks': p.remarks
+        }
       },
       deleteProduct(productId) {
-        console.log('delete')
-        console.log(productId)
-        if(confirm ('Are you sure you want to delete?')){
-          //butangan sang method sa store sng product!!!!!!! kulang pani
-        this.axios.delete('/api/product/delete?id=' + productId)
-        this.$router.push({ path:'/products/'});
+        if (confirm('Are you sure you want to delete?')) {
+          this.DELETE_PRODUCT(productId).then(o => console.log(o))
         }
       }
-
+    },
+    created: function () {
+      if (this.$store.state.product.products.length === 0) {
+        console.log('create')
+        this.$store.dispatch('INIT_PRODUCTS')
+        // this.INIT_PRODUCTS
+      } else {
+        console.log('wala nag create')
+      }
     },
     components: {
       appHeader: Header,
